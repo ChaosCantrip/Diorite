c9bd8c6613cfe67a1e76207c54c5d058d9862b44
ChaosCantrip <dev@chaoscantrip.com>
2024-02-23 08:37:01 +0000

    Added launcher.py

---
+++ launcher.py
@@ -0,0 +1,18 @@
+import os
+
+print("Starting Diorite Launcher Script")
+
+while True:
+    os.system("sudo git pull")
+
+    if os.path.exists("reboot"):
+        # Reboot the bot
+        os.remove("reboot")
+        os.system("sudo reboot")
+    elif os.path.exists("restart"):
+        # Restart the bot
+        os.remove("restart")
+        os.system("sudo python3 main.py")
+    else:
+        # Enter maintenance mode
+        os.system("sudo python3 maintenance.py")
