50c6b61583f38b07a4280956435998640efba202
ChaosCantrip <dev@chaoscantrip.com>
2024-03-14 12:27:52 +0000

    Added Champion class constructor

--- Diorite/League/Champion.py
+++ Diorite/League/Champion.py
@@ -1,2 +1,12 @@
+from Diorite.League import Skin
+
+
 class Champion:
-    pass
+
+    def __init__(self, data: dict):
+        self.id: str = data["id"]
+        self.key: int = int(data["key"])
+        self.name: str = data["name"]
+        self.title: str = data["title"]
+        self.image_url: str = r"https://ddragon.leagueoflegends.com/cdn/14.5.1/data/en_US/champion/" + data["id"] + ".png"
+        self.skins: list[Skin] = [Skin(skin) for skin in data["skins"]]
