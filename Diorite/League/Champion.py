651322225a567a3f8fe13557c0afe543e05a8a71
ChaosCantrip <dev@chaoscantrip.com>
2024-03-14 12:33:44 +0000

    Added Champion.load_champions()

--- Diorite/League/Champion.py
+++ Diorite/League/Champion.py
@@ -1,5 +1,13 @@
 from Diorite.League import Skin
 from typing import Self
+import json
+import os
+
+CHAMPIONS_JSON_PATH = "data/live/league/champions.json"
+CHAMPIONS_FOLDER_PATH = "data/live/league/champions"
+
+if not os.path.exists(CHAMPIONS_FOLDER_PATH):
+    os.makedirs(CHAMPIONS_FOLDER_PATH)
 
 
 class Champion:
@@ -21,3 +29,15 @@ class Champion:
             return cls.champions_dict[champion_id]
         else:
             raise ValueError(f"Champion {champion_id} not found.")
+
+    @classmethod
+    def load_champions(cls) -> None:
+        cls.champions.clear()
+        cls.champions_dict.clear()
+        for filename in os.listdir("data/live/league/champions"):
+            if filename.endswith(".json"):
+                with open(f"Diorite/League/champions/{filename}", "r") as f:
+                    data = json.load(f)
+                    champion = cls(data)
+                    cls.champions.append(champion)
+                    cls.champions_dict[champion.id.lower()] = champion
