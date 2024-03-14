7d86e30e60bd35bcc50946659d5ba7cccd17858f
ChaosCantrip <dev@chaoscantrip.com>
2024-03-14 12:36:31 +0000

    Added Skin __repr__

--- Diorite/League/Skin.py
+++ Diorite/League/Skin.py
@@ -5,3 +5,6 @@ class Skin:
         self.num: int = data["num"]
         self.name: str = data["name"]
         self.has_chromas: bool = data["chromas"]
+
+    def __repr__(self):
+        return f"<Skin {self.num}: {self.name} {'(chromas)' if self.has_chromas else ""}>"
