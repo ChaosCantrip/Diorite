93a920c16e75b277e7f0b530bcc78953ba79aa51
ChaosCantrip <dev@chaoscantrip.com>
2024-03-14 12:25:14 +0000

    Added Skin class constructor

--- Diorite/League/Skin.py
+++ Diorite/League/Skin.py
@@ -1,2 +1,7 @@
 class Skin:
-    pass
+
+    def __init__(self, data: dict):
+        self.id: int = int(data["id"])
+        self.num: int = data["num"]
+        self.name: str = data["name"]
+        self.has_chromas: bool = data["chromas"]
